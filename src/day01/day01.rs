1563fd4739ff86ef8e7617a285799b204df7f584
Akos Krivachy <akos.krivachy@gmail.com>
2022-12-02 12:46:47 +0000

    Formatting

--- src/day01/day01.rs
+++ src/day01/day01.rs
@@ -4,16 +4,16 @@ use std::fs;
 
 struct Accumulator {
     top3: [i32; 3],
-    current: i32
+    current: i32,
 }
 
 fn main() {
-    let contents = fs::read_to_string("src/day01/input.txt")        .expect("failed to open input.txt");
+    let contents = fs::read_to_string("src/day01/input.txt").expect("failed to open input.txt");
     let lines = contents.lines();
     let result = lines.fold(Accumulator {
         top3: [0, 0, 0],
-        current: 0
-    }, |acc: Accumulator, next: &str|  {
+        current: 0,
+    }, |acc: Accumulator, next: &str| {
         if next == "" {
             let mut top4 = vec![acc.current];
             top4.extend_from_slice(&acc.top3);
@@ -22,12 +22,12 @@ fn main() {
             top4.truncate(3);
             Accumulator {
                 top3: <[i32; 3]>::try_from(top4).unwrap(),
-                current: 0
+                current: 0,
             }
         } else {
             Accumulator {
                 top3: acc.top3,
-                current: acc.current + next.parse::<i32>().unwrap()
+                current: acc.current + next.parse::<i32>().unwrap(),
             }
         }
     });
